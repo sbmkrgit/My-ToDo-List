8e11c3fea8365a43f497f81f05f7483ba57c88c2
Shubham <babukumar9931@gmail.com>
2020-09-17 19:11:53 +0530

    updated UI

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,6 @@ import 'package:flutter/material.dart';
 import 'homepage.dart';
 
 void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          brightness: Brightness.dark,
-          primaryColor: Colors.indigoAccent,
-          accentColor: Colors.indigoAccent),
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     ));
-
